26b30242f924a6073369e105becec6677f7ba8fb
Nazmulhudashawon <shawonnazmulhuda@gmail.com>
2021-11-13 21:49:46 +0600

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId=require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -48,6 +49,15 @@ async function run() {
       console.log(result);
       res.json(result);
     });
+
+    //delete api
+app.delete('/order/:id', async (req, res)=>{
+  const id=req.params.id;
+  const query= {_id: ObjectId(id) }
+  const result=await ordersCollection.deleteOne(query);
+  console.log("deleting id", result);
+  res.json(result)
+}) 
 
     //user data from client site
     app.post('/users', async (req, res) => {
