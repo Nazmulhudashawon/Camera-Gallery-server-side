4fa3ef711ae2631c5a0f01256ff7a7101219a105
Nazmulhudashawon <shawonnazmulhuda@gmail.com>
2021-11-13 19:27:15 +0600

    first commit

--- index.js
+++ index.js
@@ -49,18 +49,50 @@ async function run() {
       res.json(result);
     });
 
-    //user from client site
+    //user data from client site
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       console.log(result);
       res.json(result);
   });
-  } finally {
-    // await client.close();
-  }
-}
+    //user data by google sign in
+  app.put('/users', async (req, res) => {
+    const user = req.body;
+    console.log(user)
+    const filter = { email: user.email };
+    const options = { upsert: true };
+    const updateDoc = { $set: user };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    res.json(result);
+});
 
+//make admin
+app.put('/users/admin',  async (req, res) => {
+  const user = req.body;
+  const filter = { email: user.email };
+          const updateDoc = { $set: { role: 'admin' } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+  })
+
+  app.get('/users/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    let isAdmin = false;
+    if (user?.role === 'admin') {
+        isAdmin = true;
+    }
+    res.json({ admin: isAdmin });
+})
+
+
+}
+finally {
+// await client.close();
+}
+}
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
